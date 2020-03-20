d8f42d892283f6d66431d2ffdddf4423314ef067
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-20 07:12:45 -0400

    Read value for a,b,c

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo " Welcome to Arithmatic Computation and sorting  Program "
+
+
+read -p "Enter value for a : " a
+read -p "Enter value for b : " b
+read -p "Enter value for c : " c
+
+
