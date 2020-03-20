34e5fb956414ebd7d9029a4925aac07ff6b4ce0c
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-20 07:43:21 -0400

    ADDED : UC3 compute Expression 2

--- ArithmeticComp.sh
+++ ArithmeticComp.sh
@@ -2,10 +2,13 @@
 
 echo " Welcome to Arithmatic Computation and sorting  Program "
 
-
+#Read value for a,b,c uc1
 read -p "Enter value for a : " a
 read -p "Enter value for b : " b
 read -p "Enter value for c : " c
 
+#Compute  first Expression uc2
 resultExp1=$(( a + b * c ))
 
+#Compute second Expression uc3
+resultExp2=$(( a * b + c ))
